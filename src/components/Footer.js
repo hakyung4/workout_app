7fe8d99ec2e87852f681cf63169568acd5250271
hakyung4 <hakyung4@illinois.edu>
2022-07-25 22:11:27 -0500

    edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
     return (
         <Box mt="80px" bgcolor="#fff3f4">
             <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-                <img src={Logo} alt="logo" width="200px" height="40px" />
-                <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
+                <Typography sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center">
+                    Have fun working out!
+                </Typography>
+                <Typography sx={{ fontSize: { lg: '28px', xs: '20px' } }} textAlign="center" pb="40px">
                     By Hakyung (Peter) Yun
                 </Typography>
             </Stack>
